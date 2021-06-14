371ac00c751c570839a029b8a93c6c987f64d42b
yondering <33736622+yonderbread@users.noreply.github.com>
2021-06-14 13:25:26 -0700

    :hammer: fix zrythm build dependency name
    Signed-off-by: yondering <33736622+yonderbread@users.noreply.github.com>

--- src/scripts/comp/zrythm.sh
+++ src/scripts/comp/zrythm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y git libxxhash binutils cmake libcurl4-gnutls-dev libgtk-3-dev libjack0 libjack-dev libsndfile1-dev meson debhelper devscripts dpkg-dev build-essential ninja-build gettext guile-2.2-dev help2man python3-sphinx ladspa-sdk libfftw3-dev libasound2-dev libavcodec-dev libchromaprint-dev libgtksourceview-3.0-dev libgraphviz-dev libpcre2-dev libsamplerate0-dev libyaml-dev librubberband-dev libzstd-dev librsvg2-dev python-is-python3 python3-pip xdg-utils rsync desktop-file-utils libsdl2-dev lv2-dev liblilv-dev portaudio19-dev librtaudio-dev zip wget unzip
+sudo apt update && sudo apt install -y git libxxhash-dev binutils cmake libcurl4-gnutls-dev libgtk-3-dev libjack0 libjack-dev libsndfile1-dev meson debhelper devscripts dpkg-dev build-essential ninja-build gettext guile-2.2-dev help2man python3-sphinx ladspa-sdk libfftw3-dev libasound2-dev libavcodec-dev libchromaprint-dev libgtksourceview-3.0-dev libgraphviz-dev libpcre2-dev libsamplerate0-dev libyaml-dev librubberband-dev libzstd-dev librsvg2-dev python-is-python3 python3-pip xdg-utils rsync desktop-file-utils libsdl2-dev lv2-dev liblilv-dev portaudio19-dev librtaudio-dev zip wget unzip
 python -m pip install --upgrade meson
 export PATH=$PATH:$HOME/.local/bin
 git clone --recursive https://github.com/zrythm/zrythm
